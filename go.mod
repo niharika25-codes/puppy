976d4198d05f45b7a7f1b258e9c37e3dd5164509
Niharika <niharika.pendem@gmail.com>
2024-06-27 00:21:35 +0530

    bigbark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/niharika25-codes/puppy
 
 go 1.22.1
+
+require github.com/niharika25-codes/dog v0.0.0-20240626182938-221900956905 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/niharika25-codes/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
